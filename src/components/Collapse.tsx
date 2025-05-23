f84036b7bb488ce4836a9fd90606047b33773f0e
Jason Entgelmeier <jasonentgelmeier@Emilys-MacBook-Pro-2.local>
2025-05-23 18:19:21 -0400

    fix type errors in Collapse element

--- src/components/Collapse.tsx
+++ src/components/Collapse.tsx
@@ -2,31 +2,40 @@ import { faAngleDown, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useContext, useEffect, useState } from "react";
 import { NodeContext, PrefillContext } from "../context/ContextProvider";
+import { DataSource, SetString } from "../types";
+
+interface CollapseProps {
+  fieldName: string;
+  setFieldName: SetString;
+  displayedDataSources: DataSource[];
+  source: DataSource;
+}
 
 function Collapse({
-  source = {},
+  source,
   fieldName,
   setFieldName,
   displayedDataSources,
-}) {
+}: CollapseProps) {
   const { elements, id, title } = source;
 
-  const [down, setDown] = useState();
+  const [down, setDown] = useState<Boolean>();
   const { prefill, setPrefill } = useContext(PrefillContext);
   const { nodeId } = useContext(NodeContext);
 
   useEffect(() => {
     const collapseElement = document.getElementById("collapse-" + id);
     if (collapseElement) {
-      setDown([...collapseElement.classList].includes("show"));
+      setDown(Array.from(collapseElement.classList).includes("show"));
     }
   }, [displayedDataSources]);
 
-  function handleClick(event) {
-    const { innerText } = event.target;
+  function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
+    const { innerText } = event.target as HTMLButtonElement;
     const field = fieldName.split(".")[1];
 
     const prefillCopy = { ...prefill };
+    // @ts-ignore
     prefillCopy[nodeId][field] = title + "." + innerText;
     setPrefill(prefillCopy);
     setFieldName("");
